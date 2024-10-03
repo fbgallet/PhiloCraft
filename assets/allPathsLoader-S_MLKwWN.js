231237d914ea32eb50a91fb138ab0c23706d975e
fbgallet <74436347+fbgallet@users.noreply.github.com>
2024-10-03 21:14:52 +0000

    Build: (c746d3f) fetch proper key variable

---
+++ assets/allPathsLoader-S_MLKwWN.js
@@ -0,0 +1,7 @@
+import{_ as o,a as n,b as i}from"./index-UNcYICgy.js";var _=function(e,a){return o(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,i(()=>import("./allPaths-rxhf2jtf.js"),__vite__mapDeps([0,1,2,3,4]))];case 1:return t=r.sent().getIconPaths,[2,t(e,a)]}})})};export{_ as allPathsLoader};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/allPaths-rxhf2jtf.js","assets/index-sRaw3GMH.js","assets/index-zdDrNEVA.js","assets/index-UNcYICgy.js","assets/index-sVd-Erbf.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
