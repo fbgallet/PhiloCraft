dc4386cbb5db61f2be3891610a9516126d77cab7
fbgallet <74436347+fbgallet@users.noreply.github.com>
2024-10-05 14:41:24 +0000

    Build: (856c785) test github secret again !

---
+++ assets/allPathsLoader-e-ybV_up.js
@@ -0,0 +1,7 @@
+import{_ as o,a as n,b as i}from"./index-_K83tPNK.js";var _=function(e,a){return o(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,i(()=>import("./allPaths-K8_-vJ79.js"),__vite__mapDeps([0,1,2,3,4]))];case 1:return t=r.sent().getIconPaths,[2,t(e,a)]}})})};export{_ as allPathsLoader};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/allPaths-K8_-vJ79.js","assets/index-sRaw3GMH.js","assets/index-zdDrNEVA.js","assets/index-_K83tPNK.js","assets/index-sVd-Erbf.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
