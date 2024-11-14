e76b1d8a0050fc10cbe24296bd9c2ebec5b8ab24
fbgallet <74436347+fbgallet@users.noreply.github.com>
2024-11-14 17:03:01 +0000

    Build: (7e9d556) Filter to current field + small refactoring

--- assets/allPathsLoader-p1hMtMhD.js
+++
@@ -1,7 +0,0 @@
-import{_ as o,a as n,b as i}from"./index-po31iJAo.js";var _=function(e,a){return o(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,i(()=>import("./allPaths-F_dbGSf2.js"),__vite__mapDeps([0,1,2,3,4]))];case 1:return t=r.sent().getIconPaths,[2,t(e,a)]}})})};export{_ as allPathsLoader};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/allPaths-F_dbGSf2.js","assets/index-sRaw3GMH.js","assets/index-zdDrNEVA.js","assets/index-po31iJAo.js","assets/index-yrB-Fxic.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
