6f3f2e62affec5942227722462ace1c45722ed83
fbgallet <74436347+fbgallet@users.noreply.github.com>
2024-11-16 22:11:11 +0000

    Build: (49ec3db) small fix: node color for new node, small changes in help dialog.

---
+++ assets/allPathsLoader-1j6oQrWZ.js
@@ -0,0 +1,7 @@
+import{_ as o,a as n,b as i}from"./index-G4J6gqne.js";var _=function(e,a){return o(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,i(()=>import("./allPaths-jmt2Uz0F.js"),__vite__mapDeps([0,1,2,3,4]))];case 1:return t=r.sent().getIconPaths,[2,t(e,a)]}})})};export{_ as allPathsLoader};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/allPaths-jmt2Uz0F.js","assets/index-sRaw3GMH.js","assets/index-zdDrNEVA.js","assets/index-G4J6gqne.js","assets/index-ehvvFClR.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
