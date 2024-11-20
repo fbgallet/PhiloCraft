710f1a06339b92e49be509015a2cf4ca8c9a20ce
fbgallet <74436347+fbgallet@users.noreply.github.com>
2024-11-20 11:31:20 +0000

    Build: (ff553de) small fix on logic.includes

--- assets/allPathsLoader-WyPGw3jc.js
+++
@@ -1,7 +0,0 @@
-import{_ as o,a as n,b as i}from"./index-mpj98DnM.js";var _=function(e,a){return o(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,i(()=>import("./allPaths-F_bZxTnv.js"),__vite__mapDeps([0,1,2,3,4]))];case 1:return t=r.sent().getIconPaths,[2,t(e,a)]}})})};export{_ as allPathsLoader};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/allPaths-F_bZxTnv.js","assets/index-sRaw3GMH.js","assets/index-zdDrNEVA.js","assets/index-mpj98DnM.js","assets/index-p_Gyp9vu.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
