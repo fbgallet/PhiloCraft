38f399c75db5827889901acb7a9b91d57a22ee30
fbgallet <74436347+fbgallet@users.noreply.github.com>
2024-11-20 22:22:19 +0000

    Build: (40770a0) Better css for mobile & fix first time basic concepts loading

--- assets/allPathsLoader-5aEwd-T-.js
+++
@@ -1,7 +0,0 @@
-import{_ as o,a as n,b as i}from"./index-ivqm3ela.js";var _=function(e,a){return o(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,i(()=>import("./allPaths-Gks0j7VL.js"),__vite__mapDeps([0,1,2,3,4]))];case 1:return t=r.sent().getIconPaths,[2,t(e,a)]}})})};export{_ as allPathsLoader};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/allPaths-Gks0j7VL.js","assets/index-sRaw3GMH.js","assets/index-zdDrNEVA.js","assets/index-ivqm3ela.js","assets/index-l3uNta6t.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
